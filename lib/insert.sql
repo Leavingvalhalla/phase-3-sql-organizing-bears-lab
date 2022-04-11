1ff7006d438b8cc14d3ade49bf1329a9ebe0e4e0
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-10 18:38:11 -0700

    commit tree d92302d897fda965283afcfbdcc5a04e749f6491

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', '2', 'M', 'brown', 'sleepy', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', '3', 'M', 'black', 'rowdy', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', '1', 'F', 'black', 'playful', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', '5', 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', '4', 'black', 'F', 'hungry', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', '3', 'green', 'M', 'stingy', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', '2', 'brown', 'F', 'sweet', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'black', 'M', 'shy', '1');
